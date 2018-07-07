98afa300798a856aec100062d052e57d8009a4f8
progfay <shun1856shun@gmail.com>
2018-07-07 17:41:08 +0900

    feat(connect): add template file of connection React and Redux

---
+++ Spotlight_toggle_imitation/redux/index.js
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import actions from './actions'
+import App from '../components/App'
+
+const mapStateToProps = state => ({
+  count: state.count
+})
+
+const mapDispatchToProps = dispatch => ({
+  countup: bindActionCreators(actions.countUp, dispatch),
+  countdown: bindActionCreators(actions.countDown, dispatch)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
