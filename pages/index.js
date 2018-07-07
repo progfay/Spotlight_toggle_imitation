d38c376c8cdcefe2d260cd64199a11b7329aa2bf
progfay <shun1856shun@gmail.com>
2018-07-07 18:56:03 +0900

    fix: change importation path

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
-import React from 'react'
 import { Provider } from 'react-redux'
-import { render } from 'react-dom'
 import createStore from '../redux/store'
 import App from '../redux'
 
-render(
+export default () => (
   <Provider store={createStore()}>
     <App />
-  </Provider>,
-  document.getElementById('root'))
+  </Provider>
+)
