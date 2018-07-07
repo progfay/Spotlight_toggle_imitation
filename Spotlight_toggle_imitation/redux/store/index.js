591c1707456220daecc5ec40c2d118d489f072a5
progfay <shun1856shun@gmail.com>
2018-07-07 17:34:36 +0900

    feat(store): add template store file with redux-logger

---
+++ Spotlight_toggle_imitation/redux/store/index.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware } from 'redux'
+import { createLogger } from 'redux-logger'
+
+import reducer from '../reducers'
+
+const logger = createLogger({
+  collapsed: true,
+  duration: true
+})
+
+export default (initialState) => (
+  createStore(
+    reducer,
+    initialState,
+    applyMiddleware(logger)
+  )
+)
