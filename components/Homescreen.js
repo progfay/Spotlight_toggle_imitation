f4ff66817c6dfa5e0452d756da3688f8fbd62004
progfay <shun1856shun@gmail.com>
2018-07-13 12:33:55 +0900

    feat(homescreen): separate statusbar

--- components/Homescreen.js
+++ components/Homescreen.js
@@ -1,6 +1,7 @@
 export default (props) => (
   <React.Fragment>
     <img src="/static/images/homescreen.png" alt="" id="homescreen"/>
+    <img src="/static/images/statusbar.png" alt="" id="statusbar"/>
     <style jsx>{`
       #homescreen {
         position: fixed;
@@ -9,7 +10,16 @@ export default (props) => (
         width: 100vw;
         height: 100vh;
         z-index: -1;
-        filter: blur(30px);
+        filter: blur(40px) brightness(65%);
+      }
+
+      #statusbar {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: auto;
+        z-index: -1;
       }
     `}</style>
   </React.Fragment>
