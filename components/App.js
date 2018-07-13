3bfe60a098b9b137791827501b2d7e2f47235b63
progfay <shun1856shun@gmail.com>
2018-07-13 13:17:29 +0900

    feat(App): compose UI components

--- components/App.js
+++ components/App.js
@@ -1,1 +1,30 @@
-export default () => (<h1> Hello world! </h1>)
+import Keyboard from './Keyboard'
+import Homescreen from './Homescreen'
+import Spotlight from './Spotlight'
+
+export default () => (
+  <React.Fragment>
+    <style jsx global>{`
+      html, body {
+        width: 100vw;
+        height: 100vh;
+      }
+
+      body {
+        position: fixed;
+        top: 0;
+        left: 0;
+        overflow: hidden;
+      }
+
+      * {
+        padding: 0;
+        margin: 0;
+        box-sizing: content-box;
+      }
+    `}</style>
+    <Homescreen />
+    <Spotlight />
+    <Keyboard />
+  </React.Fragment>
+)
