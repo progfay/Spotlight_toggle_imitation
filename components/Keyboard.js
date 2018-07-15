8cd80b91130dde3af1290e571cf99dd61ffbe9f9
progfay <shun1856shun@gmail.com>
2018-07-15 12:15:05 +0900

    feat(Keyboard): show/hide animation with CSS

--- components/Keyboard.js
+++ components/Keyboard.js
@@ -1,13 +1,22 @@
 export default (props) => (
   <React.Fragment>
-    <img src="/static/images/keyboard.png" alt="" id="keyboard"/>
+    <img src="/static/images/keyboard.png" alt="" id="keyboard"
+      className={props.visibleKeyboard ? 'open' : 'close'} />
     <style jsx>{`
       #keyboard {
         position: absolute;
-        bottom: 0px;
         width: 100vw;
-        height: auto;
+        height: 67.5vw;
         z-index: 3;
+        transition: bottom 200ms ease-out 0s;
+      }
+
+      #keyboard.open {
+        bottom: 0;
+      }
+
+      #keyboard.close {
+        bottom: -67.5vw;
       }
     `}</style>
   </React.Fragment>
