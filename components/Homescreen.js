98bce04f1c5743a4ad89b736228cbf2a107083cd
progfay <shun1856shun@gmail.com>
2018-07-15 17:55:27 +0900

    feat(Homescreen): filter animation with CSS

--- components/Homescreen.js
+++ components/Homescreen.js
@@ -3,11 +3,12 @@ const BRIGHTNESS_MAX = 65
 
 const constrain = (value, min, max) => (value < min ? min : (value > max ? max : value))
 
-export default ({ startPosY, posY, height }) => {
+export default ({ isTouching, visibleSpotlight, startPosY, posY, height }) => {
   const percentage = constrain((posY - startPosY) / height, 0, 0.35)
   return (
   <React.Fragment>
-    <img src="/static/images/homescreen.png" alt="" id="homescreen"/>
+    <img src="/static/images/homescreen.png" alt="" id="homescreen"
+      className={isTouching || visibleSpotlight ? 'filter' : 'none'} />
     <img src="/static/images/statusbar.png" alt="" id="statusbar"/>
     <style jsx>{`
       #homescreen {
@@ -17,7 +18,15 @@ export default ({ startPosY, posY, height }) => {
         width: 100vw;
         height: 100vh;
         z-index: 1;
+        transition: filter 300ms ease;
+      }
+
+      #homescreen.filter {
         filter: blur(${percentage * BLUR_MAX}px) brightness(${100 - percentage * (100 - BRIGHTNESS_MAX)}%);
+      }
+
+      #homescreen.none {
+        filter: none;
       }
 
       #statusbar {
