7b289e70db4541bc09e3bbbdcadd33eb974664d2
progfay <shun1856shun@gmail.com>
2018-07-15 19:40:59 +0900

    feat: update global CSS

--- components/App.js
+++ components/App.js
@@ -5,16 +5,15 @@ import Spotlight from './Spotlight'
 
 export default (props) => (
   <React.Fragment>
-    <Head />
-    <div
-      onTouchStart={(e) => { e.persist(); props.touchStart(e.touches[0].clientY, e.target.clientHeight) }}
-      onTouchMove={(e) => { e.persist(); props.touchMove(e.touches[0].clientY) }}
-      onTouchEnd={() => { props.touchEnd() }}
-      >
-      <style jsx global>{`
+    <Head>
+      <meta name="viewport" content="width=device-width" />
+      <meta content='width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;' name='viewport' />
+    </Head>
+    <style jsx global>{`
         html, body {
           width: 100%;
           height: 100%;
+          overscroll-behavior-y: none;
         }
 
         body {
@@ -29,9 +28,24 @@ export default (props) => (
           margin: 0;
         }
       `}</style>
+    <div id='app'
+      onTouchStart={(e) => { e.persist(); props.touchStart(e.touches[0].clientY, e.target.clientHeight) }}
+      onTouchMove={(e) => { e.persist(); props.touchMove(e.touches[0].clientY) }}
+      onTouchEnd={() => { props.touchEnd() }}
+      >
       <Homescreen {...props} />
       <Spotlight {...props} />
       <Keyboard {...props} />
     </div>
+    <style jsx>{`
+      #id {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 10;
+      }
+    `}</style>
   </React.Fragment>
 )
