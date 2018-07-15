dc5a7ecaa09e541136536b52f521a4f1892d635a
progfay <shun1856shun@gmail.com>
2018-07-15 18:54:02 +0900

    feat: add Header Component

--- components/App.js
+++ components/App.js
@@ -1,35 +1,37 @@
+import Head from './head'
 import Keyboard from './Keyboard'
 import Homescreen from './Homescreen'
 import Spotlight from './Spotlight'
 
-export default (props) => {
-  return (
-  <div
-    onTouchStart={(e) => { e.persist(); props.touchStart(e.touches[0].clientY, e.target.clientHeight) }}
-    onTouchMove={(e) => { e.persist(); props.touchMove(e.touches[0].clientY) }}
-    onTouchEnd={() => { props.touchEnd() }}
-    >
-    <style jsx global>{`
-      html, body {
-        width: 100vw;
-        height: 100vh;
-      }
+export default (props) => (
+  <React.Fragment>
+    <Head />
+    <div
+      onTouchStart={(e) => { e.persist(); props.touchStart(e.touches[0].clientY, e.target.clientHeight) }}
+      onTouchMove={(e) => { e.persist(); props.touchMove(e.touches[0].clientY) }}
+      onTouchEnd={() => { props.touchEnd() }}
+      >
+      <style jsx global>{`
+        html, body {
+          width: 100%;
+          height: 100%;
+        }
 
-      body {
-        position: fixed;
-        top: 0;
-        left: 0;
-        overflow: hidden;
-      }
+        body {
+          position: fixed;
+          top: 0;
+          left: 0;
+          overflow: hidden;
+        }
 
-      * {
-        padding: 0;
-        margin: 0;
-        box-sizing: content-box;
-      }
-    `}</style>
-    <Homescreen {...props} />
-    <Spotlight {...props} />
-    <Keyboard {...props} />
-  </div>
-)}
+        * {
+          padding: 0;
+          margin: 0;
+        }
+      `}</style>
+      <Homescreen {...props} />
+      <Spotlight {...props} />
+      <Keyboard {...props} />
+    </div>
+  </React.Fragment>
+)
