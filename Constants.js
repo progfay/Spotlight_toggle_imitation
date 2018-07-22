9eddb3b94200fb5905fb4ab3a69b1dd72a5802ad
progfay <shun1856shun@gmail.com>
2018-07-22 16:19:21 +0900

    feat(CSS props): arrange CSS animation property

--- Constants.js
+++ Constants.js
@@ -6,7 +6,7 @@ const HOMESCREEN = {
   BLUR_MAX: 40 /* px */,
   BRIGHTNESS_MAX: 65 /* % */,
   TRANSITION_DURATION: 295 /* ms */,
-  TRANSITION_TIMING_FUNCTION: 'ease-in-out'
+  TRANSITION_TIMING_FUNCTION: 'cubic-bezier(0.42, 0, 0.58, 1.0)'
 }
 
 const KEYBOARD = {
@@ -16,7 +16,7 @@ const KEYBOARD = {
 
 const SPOTLIGHT = {
   TRANSITION_DURATION: 174 /* ms */,
-  TRANSITION_TIMING_FUNCTION: 'ease-in'
+  TRANSITION_TIMING_FUNCTION: 'cubic-bezier(0.42, 0, 1.0, 1.0)'
 }
 
 export { COMMON, HOMESCREEN, KEYBOARD, SPOTLIGHT }
