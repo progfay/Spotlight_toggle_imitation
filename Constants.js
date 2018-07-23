144f504618cc55a2fb5b1f5ef6b7d7060ca12488
progfay <shun1856shun@gmail.com>
2018-07-23 16:56:25 +0900

    feat: arrange constant valiables

--- Constants.js
+++ Constants.js
@@ -1,12 +1,12 @@
 const COMMON = {
-  SWIPE_DOWN_PERSENTAGE: 35 /* % */,
+  SWIPE_DOWN_PERSENTAGE: 25 /* % */,
 }
 
 const HOMESCREEN = {
-  BLUR_MAX: 40 /* px */,
-  BRIGHTNESS_MAX: 65 /* % */,
-  TRANSITION_DURATION: 295 /* ms */,
-  TRANSITION_TIMING_FUNCTION: 'cubic-bezier(0.42, 0, 0.58, 1.0)'
+  BLUR_MAX: 30 /* px */,
+  BRIGHTNESS_MAX: 75 /* % */,
+  TRANSITION_DURATION: 278 /* ms */,
+  TRANSITION_TIMING_FUNCTION: 'cubic-bezier(0.38, 0, 0.62, 1.0)'
 }
 
 const KEYBOARD = {
