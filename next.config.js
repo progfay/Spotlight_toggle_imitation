9fed4c737d2f0d9af04aee004f810cf03aaed2e4
progfay <shun1856shun@gmail.com>
2018-07-25 00:44:50 +0900

    feat(PWAMP): apply PWA and AMP :tada:

--- next.config.js
+++ next.config.js
@@ -1,10 +1,34 @@
-module.exports = {
-  webpack: (config) => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
+const withManifest = require('next-manifest')
+const NextWorkboxWebpackPlugin = require('next-workbox-webpack-plugin')
 
+module.exports = withManifest({
+  webpack: (config, {isServer, dev, buildId, config: {distDir}}) => {
+    if (!isServer && !dev) {
+      config.plugins.push(new NextWorkboxWebpackPlugin({
+        distDir,
+        buildId
+      }))
+    }
+    config.node = { fs: 'empty' }
     return config
+  },
+  exportPathMap: () => {
+    return {
+      '/': { page: '/' }
+    }
+  },
+  manifest: {
+    'name': 'imitate Spotlight',
+    'short_name': 'imitate Spotlight',
+    'description': 'Imitation of iPhone\'s Spotlight toggle interaction',
+    'start_url': '/',
+    'display': 'standalone',
+    'orientation': 'portrait',
+    'background_color': '#FFF',
+    'theme_color': '#FFF',
+    'icon': {
+      'src': './icon/icon.png',
+      'cache': true
+    }
   }
-}
+})
